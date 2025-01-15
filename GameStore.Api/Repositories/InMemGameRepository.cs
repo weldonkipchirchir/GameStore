4f3238a9b5b7b4de2292235046146e3898f09450
wkiprono <weldonkipchirchir23@gmail.com>
2025-01-15 21:41:48 +0300

    feat: implement in-memory game storage with CRUD operations in InMemGameRepository

--- GameStore.Api/Repositories/InMemGameRepository.cs
+++ GameStore.Api/Repositories/InMemGameRepository.cs
@@ -1,6 +1,76 @@
+using GameStore.Api.Entities;
+
 namespace GameStore.Api.Repositories;
 
 public class InMemGameRepository
 {
+    private readonly List<Game?> games =
+    [
+        new Game()
+        {
+            Id = 1,
+            Name = "Street Fighter II",
+            Genre = "Fighting",
+            Price = 19.99M,
+            ReleaseDate = new DateTime(1991, 02, 01),
+            ImageUrl = "https://placehold.co/100"
+        },
+
+        new Game()
+        {
+            Id = 2,
+            Name = "Final Fantasy XIV",
+            Genre = "RolePlaying",
+            Price = 59.99M,
+            ReleaseDate = new DateTime(2010, 09, 30),
+            ImageUrl = "https://placehold.co/100"
+        },
+
+        new Game()
+        {
+            Id = 3,
+            Name = "FIFA 23",
+            Genre = "Sports",
+            Price = 69.99M,
+            ReleaseDate = new DateTime(2022, 09, 27),
+            ImageUrl = "https://placehold.co/100"
+        }
+    ];
     
+    public IEnumerable<Game?> GetGames() => games;
+    
+    public Game? GetGame(int id)
+    {
+        ArgumentOutOfRangeException.ThrowIfNegative(id);
+        return games.Find(game => game != null && game.Id == id);
+    }
+
+    public void CreateGame(Game game)
+    {
+        game.Id = games.Max(game1 => game1?.Id ?? 0) + 1;
+        games.Add(game);
+    }  
+    
+    public void UpdateGame(int id, Game updatedGame)
+    {
+        ArgumentOutOfRangeException.ThrowIfNegative(id);
+        
+        var existingGame = games.Find(game => game != null && game.Id == id);
+        if (existingGame is null) return;
+        
+        existingGame.Name = updatedGame.Name;
+        existingGame.Genre = updatedGame.Genre;
+        existingGame.ReleaseDate = updatedGame.ReleaseDate;
+        existingGame.Price = updatedGame.Price;
+        existingGame.ImageUrl = updatedGame.ImageUrl;
+    }
+    
+    public void DeleteGame(int id)
+    {
+        ArgumentOutOfRangeException.ThrowIfNegative(id);
+        
+        var existingGame = games.FindIndex(game => game != null && game.Id == id);
+        if (existingGame == -1) return;        
+        games.RemoveAt(existingGame);
+    }
 }
--- GameStore.Api/obj/Debug/net9.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore.Api/obj/Debug/net9.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d30a3b1e6c259ed5ab8b01da2ef0fa0ffcd4629a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+544f149888164f467c732c834eb634b40e546ff7")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
