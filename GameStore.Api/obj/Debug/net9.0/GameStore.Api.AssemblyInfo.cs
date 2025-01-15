cb6a756e49ffd4aaf5e9d590b54e4feb957c795b
wkiprono <weldonkipchirchir23@gmail.com>
2025-01-15 20:55:36 +0300

    feat: implement CRUD operations for games in GamesEndpoints class

--- GameStore.Api/obj/Debug/net9.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore.Api/obj/Debug/net9.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e1029991dde9296f86c31fd5c6bc458442b7324")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d30a3b1e6c259ed5ab8b01da2ef0fa0ffcd4629a")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
