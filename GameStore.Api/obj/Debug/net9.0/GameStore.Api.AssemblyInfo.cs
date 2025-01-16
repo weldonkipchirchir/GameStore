d0ac87de42a593fab8ed8f0e49b3daa7406eb80f
wkiprono <weldonkipchirchir23@gmail.com>
2025-01-16 23:16:48 +0300

    chore: update assembly information and project directory paths in generated files

--- GameStore.Api/obj/Debug/net9.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore.Api/obj/Debug/net9.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+544f149888164f467c732c834eb634b40e546ff7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f3238a9b5b7b4de2292235046146e3898f09450")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
