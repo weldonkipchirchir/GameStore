18d580b131c4952e74619c43d1aa9fc612b5353d
wkiprono <weldonkipchirchir23@gmail.com>
2025-01-17 11:51:47 +0300

    feat: update GamesEndpoints to use DTOs for game data handling

--- GameStore.Api/Endpoints/GamesEndpoints.cs
+++ GameStore.Api/Endpoints/GamesEndpoints.cs
@@ -1,3 +1,4 @@
+using GameStore.Api.Dtos;
 using GameStore.Api.Entities;
 using GameStore.Api.Repositories;
 
@@ -16,29 +17,47 @@ public static class GamesEndpoints
             .WithParameterValidation();
 
 
-        group.MapGet("/", (IGameRepository repository) => repository.GetGames());
+        group.MapGet("/", (IGameRepository repository) => repository.GetGames().Select(game => game.AsDto()));
 
         group.MapGet("/{id:int}", (int id, IGameRepository repository) =>
             {
                 var game = repository.GetGame(id);
 
-                return game is null ? Results.NotFound() : Results.Ok(game);
+                return game is null ? Results.NotFound() : Results.Ok(game.AsDto());
             })
             .WithName(GetGameEndpoint);
 
-        group.MapPost("/", (Game game, IGameRepository repository) =>
+        group.MapPost("/", (CreateGameDto gameDto, IGameRepository repository) =>
         {
+            Game game = new()
+            {
+                Name = gameDto.Name,
+                Genre = gameDto.Genre,
+                Price = gameDto.Price,
+                ReleaseDate = gameDto.ReleaseDate,
+                ImageUrl = gameDto.ImageUrl
+            };
+
             repository.CreateGame(game);
-            return Results.CreatedAtRoute(GetGameEndpoint, new { id = game.Id }, game);
+
+            return Results.CreatedAtRoute(GetGameEndpoint, new { id = game.Id }, game.AsDto());
         });
 
-        group.MapPut("/{id:int}", (int id, Game updatedGame, IGameRepository repository) =>
+        group.MapPut("/{id:int}", (int id, UpdateGameDto updatedGameDto, IGameRepository repository) =>
         {
             var game = repository.GetGame(id);
 
             if (game is null) return Results.NotFound();
 
-            repository.UpdateGame(id, updatedGame);
+            game.Name = updatedGameDto.Name;
+            game.Genre = updatedGameDto.Genre;
+            game.Price = updatedGameDto.Price;
+            game.ReleaseDate = updatedGameDto.ReleaseDate;
+            game.ImageUrl = updatedGameDto.ImageUrl;
+
+            if (game is null) return Results.NotFound();
+
+            repository.UpdateGame(id, game);
 
             return Results.NoContent();
         });
