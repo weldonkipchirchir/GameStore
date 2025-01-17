4a128b27e05494a836260811d71a5bb99217c5a9
wkiprono <weldonkipchirchir23@gmail.com>
2025-01-17 11:51:57 +0300

    feat: add extension method to convert Game entities to GameDto

---
+++ GameStore.Api/Entities/EntityExtensions.cs
@@ -0,0 +1,9 @@
+using GameStore.Api.Dtos;
+
+namespace GameStore.Api.Entities
+{
+    public static class EntityExtensions
+    {
+        public static GameDto AsDto(this Game game) => new GameDto(game.Id, game.Name, game.Genre, game.Price, game.ReleaseDate, game.ImageUrl);
+    }
+}
