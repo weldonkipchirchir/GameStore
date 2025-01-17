997774909a7053b9c0a612dd06869b7f8c563c51
wkiprono <weldonkipchirchir23@gmail.com>
2025-01-17 11:11:03 +0300

    feat: register InMemGameRepository as singleton for game data management

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,7 +1,11 @@
 using GameStore.Api.Endpoints;
 using GameStore.Api.Entities;
+using GameStore.Api.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSingleton<IGameRepository, InMemGameRepository>();
+
 var app = builder.Build();
 
 app.MapGamesEndpoints();
