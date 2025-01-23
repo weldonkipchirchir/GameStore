65923df404944c91fe5cba89718f8f698593ace9
Weldon Kiprono <67738405+weldonkipchirchir@users.noreply.github.com>
2025-01-23 21:47:40 +0300

    Update: remove unnecessary id null check and use brackets instead of list

--- GameStore.Api/Repositories/InMemGameRepository.cs
+++ GameStore.Api/Repositories/InMemGameRepository.cs
@@ -4,38 +4,36 @@ namespace GameStore.Api.Repositories;
 
 public class InMemGameRepository : IGameRepository
 {
-    private readonly List<Game?> games =
-    [
-        new Game()
-        {
-            Id = 1,
-            Name = "Street Fighter II",
-            Genre = "Fighting",
-            Price = 19.99M,
-            ReleaseDate = new DateTime(1991, 02, 01, 0, 0, 0, DateTimeKind.Utc),
-            ImageUrl = "https://placehold.co/100"
-        },
-
-        new Game()
-        {
-            Id = 2,
-            Name = "Fantasy XIV",
-            Genre = "RolePlaying",
-            Price = 59.99M,
-            ReleaseDate = new DateTime(2010, 09, 30, 0, 0, 0, DateTimeKind.Utc),
-            ImageUrl = "https://placehold.co/100"
-        },
-
-        new Game()
-        {
-            Id = 3,
-            Name = "FIFA 23",
-            Genre = "Sports",
-            Price = 69.99M,
-            ReleaseDate = new DateTime(2022, 09, 27, 0, 0, 0, DateTimeKind.Utc),
-            ImageUrl = "https://placehold.co/100"
-        }
-    ];
+    private readonly List<Game?> games = new List<Game?>
+{
+    new Game()
+    {
+        Id = 1,
+        Name = "Street Fighter II",
+        Genre = "Fighting",
+        Price = 19.99M,
+        ReleaseDate = new DateTime(1991, 02, 01, 0, 0, 0, DateTimeKind.Utc),
+        ImageUrl = "https://placehold.co/100"
+    },
+    new Game()
+    {
+        Id = 2,
+        Name = "Fantasy XIV",
+        Genre = "RolePlaying",
+        Price = 59.99M,
+        ReleaseDate = new DateTime(2010, 09, 30, 0, 0, 0, DateTimeKind.Utc),
+        ImageUrl = "https://placehold.co/100"
+    },
+    new Game()
+    {
+        Id = 3,
+        Name = "FIFA 23",
+        Genre = "Sports",
+        Price = 69.99M,
+        ReleaseDate = new DateTime(2022, 09, 27, 0, 0, 0, DateTimeKind.Utc),
+        ImageUrl = "https://placehold.co/100"
+    }
+};
 
     public IEnumerable<Game?> GetGames() => games;
 
@@ -52,21 +50,16 @@ public class InMemGameRepository : IGameRepository
     }
 
     public void UpdateGame(int id, Game updatedGame)
-    {
-        if(id == null){
-            return;
-        }
-        ArgumentOutOfRangeException.ThrowIfNegative(id);
-
-        var existingGame = games.Find(game => game != null && game.Id == id);
-        if (existingGame is null) return;
-
-        existingGame.Name = updatedGame.Name;
-        existingGame.Genre = updatedGame.Genre;
-        existingGame.ReleaseDate = updatedGame.ReleaseDate;
-        existingGame.Price = updatedGame.Price;
-        existingGame.ImageUrl = updatedGame.ImageUrl;
-    }
+{
+    ArgumentOutOfRangeException.ThrowIfNegative(id);
+    var existingGame = games.Find(game => game != null && game.Id == id);
+    if (existingGame is null) return;
+    existingGame.Name = updatedGame.Name;
+    existingGame.Genre = updatedGame.Genre;
+    existingGame.ReleaseDate = updatedGame.ReleaseDate;
+    existingGame.Price = updatedGame.Price;
+    existingGame.ImageUrl = updatedGame.ImageUrl;
+}
 
     public void DeleteGame(int id)
     {
