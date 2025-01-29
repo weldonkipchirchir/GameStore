802a1be6cd5d0bcf6eac030e8e6eaa3d1607348c
wkiprono <weldonkipchirchir23@gmail.com>
2025-01-29 10:06:36 +0300

    refactor: change CreateGameDto and UpdateGameDto from abstract to regular records

--- GameStore.Api/Dtos.cs
+++ GameStore.Api/Dtos.cs
@@ -11,7 +11,7 @@ public record GameDto(
     string ImageUrl
 );
 
-public abstract record CreateGameDto(
+public record CreateGameDto(
     [Required][StringLength(50)] string Name,
     [Required][StringLength(20)] string Genre,
     [Range(1, 100)] decimal Price,
@@ -19,7 +19,7 @@ public abstract record CreateGameDto(
     [Url][StringLength(100)] string ImageUrl
 );
 
-public abstract record UpdateGameDto(
+public record UpdateGameDto(
     [Required][StringLength(50)] string Name,
     [Required][StringLength(20)] string Genre,
     [Range(1, 100)] decimal Price,
