51ccaa835c0f0fc9a90adb41d61d8d94a68c208e
wkiprono <weldonkipchirchir23@gmail.com>
2025-01-30 13:39:54 +0300

    feat: refactor Program.cs to initialize database and add repository services

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,14 +1,16 @@
+using GameStore.Api.Data;
 using GameStore.Api.Endpoints;
 using GameStore.Api.Entities;
 using GameStore.Api.Repositories;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddSingleton<IGameRepository, InMemGameRepository>();
-
-var connString = builder.Configuration.GetConnectionString("GameStoreContext");
+builder.Services.AddRepositoryServices(builder.Configuration);
 
 var app = builder.Build();
+
+await app.Services.InitializeDatabaseAsync();
 
 app.MapGamesEndpoints();
 
