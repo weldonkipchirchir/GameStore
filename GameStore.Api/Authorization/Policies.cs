888ab57ea92825526f5a7f1a5107fba42a3a7f63
wkiprono <weldonkipchirchir23@gmail.com>
2025-01-31 13:37:11 +0300

    feat: add authorization policies for read and write access

---
+++ GameStore.Api/Authorization/Policies.cs
@@ -0,0 +1,9 @@
+using System.ComponentModel;
+
+namespace GameStore.Api.Authorization;
+
+public static class Policies
+{
+    public const string ReadAccess = "read_access";
+    public const string WriteAccess = "write_access";
+}
