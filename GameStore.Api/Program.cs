86293b35b4a11ad9a6698ca8028432f90141a912
wkiprono <weldonkipchirchir23@gmail.com>
2025-02-06 17:03:16 +0300

    feat: add rate limiting and exception handling middleware to the application

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,8 +1,12 @@
+using System.Diagnostics;
 using System.Text;
+using System.Threading.RateLimiting;
 using GameStore.Api.Data;
 using GameStore.Api.Endpoints;
 using GameStore.Api.Entities;
 using GameStore.Api.Authorization;
+using GameStore.Api.Dtos.ErrorHandling;
+using GameStore.Api.Dtos.Middleware;
 using GameStore.Api.Repositories;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
@@ -16,7 +20,11 @@ builder.Services.AddRepositoryServices(builder.Configuration);
 // Add JWT authentication
 builder.Services.AddJwtBearerAuthentication(builder.Configuration);
 
-builder.Services.AddAuthorizationPolicies();    
+builder.Services.AddAuthorizationPolicies();
+
+builder.Services.RateLimiterMiddleware();
+
+//builder.Services.AddTransient<ExceptionsMiddleware>();
 
 // Build the application
 var app = builder.Build();
@@ -25,6 +33,10 @@ var app = builder.Build();
 app.UseAuthentication();
 
 app.UseAuthorization();
+
+app.UseExceptionHandler(exceptionHandlerApp => exceptionHandlerApp.ConfigureExceptionHandling());
+
+//app.UseMiddleware<ExceptionsMiddleware>();
 
 // Initialize database and map endpoints
 await app.Services.InitializeDatabaseAsync();
