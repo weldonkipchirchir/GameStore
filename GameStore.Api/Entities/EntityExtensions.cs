d0d19cbd03795fd348cfb2f26f6749a98af1f12b
wkiprono <weldonkipchirchir23@gmail.com>
2025-02-21 09:57:42 +0300

    fix: update ImageUrl to ImageUri in EntityExtensions for consistency

--- GameStore.Api/Entities/EntityExtensions.cs
+++ GameStore.Api/Entities/EntityExtensions.cs
@@ -4,8 +4,8 @@ namespace GameStore.Api.Entities
 {
     public static class EntityExtensions
     {
-        public static GameDtoV1 AsDtoV1(this Game game) => new(game.Id, game.Name, game.Genre, game.Price, game.ReleaseDate, game.ImageUrl);
+        public static GameDtoV1 AsDtoV1(this Game game) => new(game.Id, game.Name, game.Genre, game.Price, game.ReleaseDate, game.ImageUri);
 
-        public static GameDtoV2 AsDtoV2(this Game game) => new(game.Id, game.Name, game.Genre, game.Price, game.Price - (game.Price * .3m), game.ReleaseDate, game.ImageUrl);
+        public static GameDtoV2 AsDtoV2(this Game game) => new(game.Id, game.Name, game.Genre, game.Price, game.Price - (game.Price * .3m), game.ReleaseDate, game.ImageUri);
     }
 }
